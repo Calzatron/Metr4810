f8132be1bcb1859e349f6c35ea770e34ff6a142c
Calzatron <callumrohweder@gmail.com>
2017-04-01 14:54:42 +1000

    PWM code upload

---
+++ pwm_attempt.c
@@ -0,0 +1,57 @@
+/*
+ * pwm_attempt.c
+ *
+ */ 
+
+ #include <avr/io.h>
+ #include <avr/interrupt.h>			//enables interrupts
+ #include <avr/pgmspace.h>			//for storing / accessing data in flash program instead of SRAM -> variable manipulation
+ #include <stdio.h>					//input / output
+ #include <stdlib.h>					//standard function library; malloc / sort / rand etc
+ //#include <util/delay.h>
+ //#include <string.h>
+
+
+void pwm_initialiser(void){
+   /*
+   TCCR2A - Timer Counter Control Register (TIMER/COUNTER2)
+   -----------------------------------------------
+   BITS DESCRIPTION
+   NO:   NAME   DESCRIPTION
+   --------------------------
+   BIT 7 : COM2A1   Compare Output Mode for Channel A [n = 1:0] [Set to 1]
+   BIT 6 : COM2A0	"	[Set to 0]
+   BIT 5 : COM2B1	"	[Set to 1]
+   BIT 4 : COM2B0	"	[Set to 0]
+
+   BIT 3 :
+   BIT 2 :
+   BIT 1 : WGM21   Waveform Generation Mode [n = 1:0]	[SET to 1]
+   BIT 0 : WGM20   "	[SET to 1]
+
+
+    TCCR2B - TC2 Control Register B
+    -----------------------------------------------
+    BITS DESCRIPTION
+    NO:   NAME   DESCRIPTION
+    --------------------------
+    BIT 7 : FOC2A   
+    BIT 6 : FOC2B	
+    BIT 5 : COM2B1	"	[Set to 1]
+    BIT 4 : COM2B0	"	[Set to 0]
+
+    BIT 3 : WGM22	Waveform Generation Mode	[Set to 1]
+    BIT 2 : CA22	Clock Select 2 [n = 0..2] [Set to 1]
+    BIT 1 : CA21   "	[SET to 0]
+    BIT 0 : CS20   "	[SET to 0]  
+
+   --------------------------
+   The above settings are for
+   Timer Clock = CPU Clock (No Prescalling)
+   Mode        = Fast PWM
+   PWM Output  = Non Inverted
+
+
+   */
+
+}
